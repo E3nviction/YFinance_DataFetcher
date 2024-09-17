d643304b56d04555f8008f1c73047786a85082db
Enviction <schmidtjonas662@gmail.com>
2024-09-17 11:48:06 +0200

    Init

---
+++ main.py
@@ -0,0 +1,4 @@
+from src import *
+
+print(get_ticker_info("AAPL"))
+print(get_earnings("AAPL"))
---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+"""
+__init__
+"""
+from .yfinance_earner import *
+
+__all__ = [
+    "Stock",
+    "get_ticker_info",
+    "get_earnings",
+    "get_historical_earnings",
+    "get_data",
+    "get_historical_data",
+]
